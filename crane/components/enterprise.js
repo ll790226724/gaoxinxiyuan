3fd4e8922069cc52f744a0fdfcc59b3eb0ebbd2d
renhaihuang <790226724@qq.com>
2020-08-18 15:39:44 +0800

    feat: 合并组件

---
+++ crane/components/enterprise.js
@@ -0,0 +1,35 @@
+const enterpriseTable = require('./enterprise-table')
+module.exports = [
+    {
+        component: 'img',
+        position: [48, 132],
+        props: {
+            src: '/gaoxinxiyuan/images/icon/times-circle.svg',
+            $style: {
+                width: '24px',
+                height: '24px',
+            }
+        },
+        events: {
+            'click': {
+                actions: ["setState('showState', !craneStates.showState)"]
+            }
+        },
+    },
+    {
+        component: 'div',
+        position: [88, 130],
+        props: {
+            $style: {
+                width: '524px',
+                height: '28px',
+                color: '#fff',
+                fontSize: '28px',
+                lineHeight: '28px',
+                fontWeight: '600',
+            },
+            'v-text': 'craneStates.enterpriseName'
+        }
+    },
+    enterpriseTable,
+]
--- src/views/map.vue
+++ src/views/map.vue
@@ -112,7 +112,11 @@ export const map = {
         fireFightingCompanyBuildingData: [],
         dangerousChemicalCompanyBuildingData: [],
         selectAreaOptions: [],
-        currentCompany: ''
+        currentCompany: '',
+        showState: true,
+        enterpriseName: '成都中光电科技有限公司',
+        tableHeader: [{field: 'company_name', value: '企业名称'}, {field: 'registered_address', value: '注册地址'}, {field: 'actual_address', value: '实际地址'}, {field: 'industry', value: '行业领域'}, {field: 'main_business', value: '主营业务'}, {field: 'header_name', value: '单位负责人姓名'}, {field: 'header_phone', value: '单位负责人电话'}, {field: 'area', value: '场地总面积'}, {field: 'number', value: '从业人数（人）'}, {field: 'security_name', value: '安全负责人姓名'}, {field: 'sucurity_phone', value: '安全负责人电话'}],
+        safetyOfficerData: [{id: '0047dd5e-d3ff-4c5f-9ab4-44d75afb40a1', type: '安全总负责人'}, {id: '0057dd5e-d3ff-4c5f-9ab4-44d75afb40a1', type: '安全第一负责人'}, {id: '0067dd5e-d3ff-4c5f-9ab4-44d75afb40a1', type: '安全第二负责人'}],
       },
     }
   },
@@ -195,6 +199,11 @@ export const map = {
         return 'tab--active'
       }
       return 'tab--default'
+    },
+    reqUrl(id){
+      return function(id){
+        return `/v1/components/${id}/data`
+      }
     }
   },
 
